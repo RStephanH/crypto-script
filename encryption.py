4470b9930915cc3aac6e0fdce6be57b6ada74208
rsh <randrianirinahernandezstephan@gmail.com>
2025-04-13 20:29:40 +0300

    feat: class encryption

---
+++ encryption.py
@@ -0,0 +1,13 @@
+import func
+
+class encryption:
+    """Model of encryption """
+    def __init__(self):
+        concern_file=func.choose_file()
+        enc_meth=func.encryption_method()
+
+    def encrypt(self):
+        pass
+
+    def decrypt(self):
+        pass
